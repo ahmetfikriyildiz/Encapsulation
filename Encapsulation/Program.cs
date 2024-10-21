281e437a65f0f83774d439b38225db9f819d5a42
ahmet fikri yıldız <ahmet_6119@outlook.com.tr>
2024-10-21 06:52:30 +0300

    i

--- Encapsulation/Program.cs
+++ Encapsulation/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Encapsulation
 {
+    // Bu uygulamada kapı sayısı özelliği üzerinden nesne yöenelimli programlamadaki kapsülleme özelliğini anlatmaya çalıştım.
     public class Araba
     {
         // Özellikler (Properties)
